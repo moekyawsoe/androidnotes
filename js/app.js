5388ca7a8dc74cc302c3c648d071346d5872426e
moekyawsoe <moekyawsoe109@gmail.com>
2020-05-30 22:14:23 +0630

    initial Commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+ $(document).ready(function () {
+   $("#sidebarCollapse").on("click", function () {
+     $("#sidebar").toggleClass("active");
+   });
+ });
+ $(document).ready(function () {
+   $("#container").load("views/main.html");
+ });
+
+ function switchView(view) {
+   $.get({
+     url: view,
+     cache: false,
+   }).then(function (data) {
+     $("#container").html(data);
+   });
+ }
