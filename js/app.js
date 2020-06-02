dd9247579d2d8298686aab3069025b8b9a22f84d
moekyawsoe <moekyawsoe109@gmail.com>
2020-06-02 19:37:34 +0630

    updated theme

--- js/app.js
+++ js/app.js
@@ -1,24 +1,31 @@
 
- $(document).ready(function () {
-   $("#sidebar").toggleClass("a active");
- });
-
- $(document).ready(function () {
-   $("#sidebarCollapse").on("click", function () {
-     
-     $("#sidebar").toggleClass("active");
-   });
- });
- $(document).ready(function () {
-   $("li.navitem").on("click", function () {
-     $("#sidebar").toggleClass("active");
-   });
- });
-
- $(document).ready(function () {
+ $(document).ready(function () { 
    $("#container").load("views/main.html");
+
+   $("#sidebar").toggleClass("a active");
+
+   $("#darkswitch").change(function () {
+     // this will contain a reference to the checkbox
+     if (this.checked) {
+       // the checkbox is now checked
+       $("#css").attr("href", "css/darkcss.css");
+     } else {
+       // the checkbox is now no longer checked
+       $("#css").attr("href", "css/style.css");
+     }
+   });
+
+    $("li.navitem").on("click", function () {
+      $("#sidebar").toggleClass("active");
+    });
+
+    $("#sidebarCollapse").on("click", function () {
+      $("#sidebar").toggleClass("active");
+    });
+
  });
 
+ 
  function switchView(view) {
    $.get({
      url: view,
@@ -27,3 +34,8 @@
      $("#container").html(data);
    });
  }
+
+//  function myFunction() {
+//    var element = document.body;
+//    element.classList.toggle("dark-mode");
+//  }
